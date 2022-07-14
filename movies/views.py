3860fbb3cae07e982cdfc2eb22872eff037c1de7
고현영 <gusdud3890@gmail.com>
2022-07-14 15:16:54 +0900

    ADD: movie post and list get api

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.forms import ValidationError
 
-# Create your views here.
+from rest_framework import generics
+
+from movies.models      import MovieList, Rating
+from movies.serializers import MovieListSerializer, RatingSerializer
+
+
+class MoviewListView(generics.ListCreateAPIView):
+    serializer_class = MovieListSerializer
+    queryset         = MovieList.objects.all()
+
+
+class RatingView(generics.ListCreateAPIView):
+    serializer_class = RatingSerializer
+    queryset = Rating.objects.all()
+
+    def perform_create(self, serializer):
+        try:
+            pk         = self.kwargs.get('pk')
+            user       = self.request.user
+            movie_list = MovieList.objects.get(pk=pk)
+
+            reviews = Rating.objects.filter(user=user, movie_list=movie_list)
+            if reviews.exists():
+                raise ValidationError('your rating already exists')
+        except MovieList.DoesNotExist:
+            raise ValidationError('movie does not exist')
+        
+        if movie_list.number_rating == 0:
+            movie_list.avg_rating = serializer.validated_data['point_rating']
+
+        movie_list.avg_rating = (movie_list.avg_rating + serializer.validated_data['point_rating']) / 2
+        
+        movie_list.number_rating = movie_list.number_rating + 1
+        movie_list.save()
+
+        serializer.save(user=user, movie_list=movie_list)
--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,11 @@
-from django.urls    import path, include
+from django.urls import path
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.views import RegistrationView
 
 urlpatterns = [
-    path('/registration', RegistrationView.as_view(), name='registration'),
-    path('/api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('/api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('registration/', RegistrationView.as_view(), name='registration'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
